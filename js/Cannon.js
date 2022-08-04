79248683ad6f357a819675808cbc776fd9642e57
Febin-Sunil <106514951+Febin-Sunil@users.noreply.github.com>
2022-08-04 19:05:18 +1000

    Add files via upload

---
+++ js/Cannon.js
@@ -0,0 +1,37 @@
+class Cannon{
+  //Properties
+  constructor(x,y,w,h,angle){
+      this.x=x;
+      this.y=y;
+      this.w=w;
+      this.h=h;
+      this.angle=angle;
+  }
+
+  //function
+  display(){
+      if(keyIsDown(LEFT_ARROW) && this.angle>-1.45){
+        this.angle = this.angle - 0.05;
+      }
+      if(keyIsDown(RIGHT_ARROW) && this.angle<0.35){
+        this.angle = this.angle + 0.05;
+      }
+    
+     console.log(this.angle)
+    
+    
+      fill("#676e6a");
+      push();
+      translate(this.x,this.y);
+      rotate(this.angle); //45
+      rect(-10,-20,this.w,this.h);
+      pop();
+
+      arc(this.x-30,this.y+90,140,200,PI,PI*2);
+      noFill();
+
+
+  }
+
+
+}
